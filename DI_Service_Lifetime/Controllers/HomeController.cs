6eb717c7542ccca677c3eaffbfbe8a22cd8ebd87
vsprojects87 <vs.scala.619@gmail.com>
2024-04-30 14:32:02 +0530

    Pending code completed

--- DI_Service_Lifetime/Controllers/HomeController.cs
+++ DI_Service_Lifetime/Controllers/HomeController.cs
@@ -1,21 +1,48 @@
 using DI_Service_Lifetime.Models;
+using DI_Service_Lifetime.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 
 namespace DI_Service_Lifetime.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ISingletonGuidService _singleton1;
+        private readonly ISingletonGuidService _singleton2;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IScopedGuidService _scoped1;
+        private readonly IScopedGuidService _scoped2;
+
+        private readonly ITransientGuidService _transient1;
+        private readonly ITransientGuidService _transient2;
+
+
+        public HomeController(IScopedGuidService scoped1,
+                              IScopedGuidService scoped2,
+                              ISingletonGuidService singleton1,
+                              ISingletonGuidService singleton2,
+                              ITransientGuidService transient1,
+                              ITransientGuidService transient2)
         {
-            _logger = logger;
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
+            _scoped1 = scoped1;
+            _scoped2 = scoped2;
+            _transient1 = transient1;
+            _transient2 = transient2;
         }
 
         public IActionResult Index()
         {
-            return View();
+            StringBuilder msg = new StringBuilder();
+            msg.Append($"Transient 1 : {_transient1.GetGuid()} \n");
+            msg.Append($"Transient 1 : {_transient2.GetGuid()} \n");
+            msg.Append($"Scoped 1 : {_scoped1.GetGuid()} \n");
+            msg.Append($"Scoped 2 : {_scoped2.GetGuid()} \n");
+            msg.Append($"Singleton 1 : {_singleton1.GetGuid()} \n");
+            msg.Append($"Singleton 2 : {_singleton2.GetGuid()} \n");
+            return View(msg.ToString());
         }
 
         public IActionResult Privacy()
